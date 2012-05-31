a06a88bc473a1c7b19167a90a71cfc4b251ed95f
chensihai <chensihai@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-31 03:23:08 +0000

    This is the 2nd updated
    git-svn-id: https://plugins.svn.wordpress.org/yii-bridge/trunk@551199 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -19,13 +19,6 @@ The shiny part is, you don’t need to do any hack in WordPress, or Yii
 framework/Application, just install the plugin, and you’ll see all Yii
 function work seamless within WordPress.
 
-This is a standard free version. You mind find some functions miss, for
-examples,
-a)Login function,
-b)Captcha
-c)Style sheet
-For a PRO version, which cover all the functions, please go to
-http://wp.69digital.com.
 
 == Installation ==
 ---and test---
@@ -53,17 +46,17 @@ b)test shortcode like 3)
 
 == Screenshots ==
 
-Please check the demos
-Yii Bridge Demos/blog
-Yii Bridge demos/hangman
-Yii Bridge Demos/Helloworld
-Yii Bridge demos/phonebook
-Yii Bridge User created app demo
+Please check the demos on http://wp.69digital.com\n
+Yii Bridge Demos/blog\n
+Yii Bridge demos/hangman\n
+Yii Bridge Demos/Helloworld\n
+Yii Bridge demos/phonebook\n
+Yii Bridge User created app demo\n
 
 == Changelog ==
 
-=0.4 =
-*This is the first release version
+=0.5 =
+*This is the 2nd release version
 * Another change.
 
 
--- yii-bridge.php
+++ yii-bridge.php
@@ -4,7 +4,6 @@
     Description: connect Yii www.yiiframework.com as a MVC framework for Wordpress
     Author: chensihai
     Version: 0.4
-    Author URI: http://wp.69digital.com/
     */
 
     function yii_bridge_func($atts){
@@ -42,4 +41,68 @@
 
     add_shortcode('yii','yii_bridge_func');
 
+    //if(strpos($_SERVER['REQUEST_URI'],"site/login")>0||strpos($_SERVER['REQUEST_URI'],"site/logout")>0)
+    {    
+        add_action('get_header', 'login_func',-100);
+    }
+    if(strpos($_SERVER['REQUEST_URI'],"captcha")>0)
+    {
+        add_action('get_header', 'captcha_func',-100);
+    }
+    function login_func(){
+        ob_start();
+    }
+
+    function captcha_func(){
+        global $posts;
+        $shortcode_found=false;
+        foreach($posts as $post){
+            if (stripos($post->post_content, '[yii ') !== false) {
+                $shortcode_found = true; 
+                $str = preg_split('!(\[.*?\])!', $post->post_content, -1,  PREG_SPLIT_DELIM_CAPTURE  );
+                $str=trim($str[1],"[");
+                $str=trim($str,"]");
+                $strs=explode("=",$str);
+                $app_path=$strs[1];
+                break;
+            }
+        } 
+        $app="yii/".$app_path;
+        $file=ABSPATH."/".$app."/index.php";
+        $_SERVER['PHP_SELF']=$_SERVER['SCRIPT_NAME']="/".$app."/index.php";            
+        $_SERVER['SCRIPT_FILENAME']=$file;
+        $_GET['r']="site/captcha";
+        require($file);
+        die();    
+    }
+
+    function header_func(){
+        global $posts;
+        $shortcode_found=false;
+        foreach($posts as $post){
+            if (stripos($post->post_content, '[yii ') !== false) {
+                $shortcode_found = true;
+                $str = preg_split('!(\[.*?\])!', $post->post_content, -1,  PREG_SPLIT_DELIM_CAPTURE  );
+                $str=trim($str[1],"[");
+                $str=trim($str,"]");
+                $strs=explode("=",$str);
+                $app_path=$strs[1];
+
+                break;
+            }
+        } 
+        if(!$shortcode_found) return;
+    ?>
+    <!-- blueprint CSS framework -->
+    <link rel="stylesheet" type="text/css" href="/yii/<?php echo $app_path ?>/css/screen.css" media="screen, projection" />
+    <link rel="stylesheet" type="text/css" href="/yii/<?php echo $app_path ?>/css/print.css" media="print" />
+    <!--[if lt IE 8]>
+    <link rel="stylesheet" type="text/css" href="/yii/<?php echo $app_path ?>/css/ie.css" media="screen, projection" />
+    <![endif]-->
+
+    <link rel="stylesheet" type="text/css" href="/yii/<?php echo $app_path ?>/css/main.css" />
+    <link rel="stylesheet" type="text/css" href="/yii/<?php echo $app_path ?>/css/form.css" />
+    <?php       
+    } 
+    add_action('wp_head', 'header_func');       
 ?>
